b6e9343e9fd65707a526d8c611f5e2a86a2bbe54
Alina <gav621@mail.ru>
2022-12-23 10:37:01 +0300

    added solution

---
+++ Task/Program.cs
@@ -0,0 +1,28 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых <=3 символам
+
+Console.WriteLine("Введите строки через пробел");
+string s = Console.ReadLine();
+string[] array = s.Split(" ");
+string[] result = new string[array.Length];
+
+void SecondArray(string[] array1, string[] array2)
+{
+int count = 0;
+for (int i = 0; i < array1.Length; i++)
+{
+    if (array1[i].Length <= 3)
+    {
+        array2[count] = array1[i];
+        count++;
+    }
+}
+}
+
+void Print(string[] a)
+{
+    for (int i = 0; i < a.Length; i++)
+        System.Console.Write($"{a[i],5}");
+}
+
+SecondArray(array,result);
+Print(result);
